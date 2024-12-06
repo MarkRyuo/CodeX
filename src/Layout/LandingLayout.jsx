28463553b1c510168ee4445c685f0a6624fa1ed9
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-06 20:06:52 +0800

    Subject: Update

--- src/Layout/LandingLayout.jsx
+++ src/Layout/LandingLayout.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 
 function LandingLayout() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
-export default LandingLayout ;
+export default LandingLayout;
