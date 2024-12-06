883fe05d9e0490bac83979260727bc9328c112da
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-06 20:01:32 +0800

    Subject: Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,5 @@
 import ReactDOM from "react-dom/client";
-import { BrowserRouter } from "react-router";
+import { BrowserRouter, Routes, Route } from "react-router";
 import './index.css'
 import App from './App'
 
@@ -7,6 +7,8 @@ const root = document.getElementById("root");
 
 ReactDOM.createRoot(root).render(
   <BrowserRouter>
-    <App />
+    <Routes>
+      <Route path="/" element={ <App />}/>
+    </Routes>
   </BrowserRouter>
 );
