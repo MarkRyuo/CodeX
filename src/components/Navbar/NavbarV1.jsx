191c90f698c854765e0cdea0cefa762cd3865cc3
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 17:33:45 +0800

    Subject: Update

--- src/components/Navbar/NavbarV1.jsx
+++ src/components/Navbar/NavbarV1.jsx
@@ -1,14 +1,18 @@
 import { Link } from "react-router";
 
 function NavbarV1() {
+
+    const LinkName = 'Link' ;
+
+
     return (
         <nav className="NavbarV1Container">
             <img src="path-to-logo" alt="Logo" />
 
             <ul className="container-wrapper">
-                <li><Link to={"/link-to"}>Link</Link></li>
-                <li><Link to={"/link-to"}>Link</Link></li>
-                <li><Link to={"/link-to"}>Link</Link></li>
+                <li><Link to={"/link-to"}>{LinkName}</Link></li>
+                <li><Link to={"/link-to"}>{LinkName}</Link></li>
+                <li><Link to={"/link-to"}>{LinkName}</Link></li>
             </ul>
         </nav>
     )
