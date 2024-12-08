1688a642865abf51b9caeec06bdeed812dab0334
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 16:24:08 +0800

    Subject: Update

--- src/components/Navbar/LandingNavbar.jsx
+++ src/components/Navbar/LandingNavbar.jsx
@@ -4,6 +4,12 @@ function LandingNavbar() {
     return (
         <nav>
             <img src="#" alt="Logo" />
+
+            <ul>
+                <li>
+                    <Link>Home</Link>
+                </li>
+            </ul>
         </nav>
     )
 }
