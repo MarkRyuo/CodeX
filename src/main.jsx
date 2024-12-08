36b31e8fd80091960ca1543c8122cfa8fe2e9b4e
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 16:00:54 +0800

    Subject: Update

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,10 @@
-import ReactDOM from "react-dom/client";
-import { BrowserRouter, Routes, Route } from "react-router";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App'
+import App from './App.jsx'
 
-const root = document.getElementById("root");
-
-ReactDOM.createRoot(root).render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={ <App />}/>
-    </Routes>
-  </BrowserRouter>
-);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
