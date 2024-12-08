72688d96c51b168bba8e8717f7fba05d3a96304d
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 16:19:20 +0800

    Subject: Update

--- src/components/Navbar/LandingNavbar.jsx
+++ src/components/Navbar/LandingNavbar.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
+
 
 function LandingNavbar() {
-  return (
-    <div>
-      
-    </div>
-  )
+    return (
+        <div>
+
+        </div>
+    )
 }
 
-export default LandingNavbar
+export default LandingNavbar;
