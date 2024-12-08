3a461b0275ccef94a61c74296c5c368959b2aa37
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 17:32:55 +0800

    Subject: Update

--- src/components/Navbar/NavbarV1.jsx
+++ src/components/Navbar/NavbarV1.jsx
@@ -3,27 +3,20 @@ import { useState } from "react";
 
 function NavbarV1() {
 
-    const [linkName] =  useState([
-        { id: 1, imgLogo: '/path-img' ,linkName: "Sample1", path: '/path-to'}
-    ]);
+    const [linkName, setLinkName] =  ;
 
 
     return (
+        <nav className="NavbarV1Container">
+            <img src="path-to-logo" alt="Logo" />
 
-        <>
-            {linkName.map((linkNames) => {
-                <nav className="container-NavbarV1" key={linkNames.id}>
-                    <img src={linkNames.imgLogo} alt="Logo" />
-
-                    <ul className="container-wrapper">
-                        <li><Link to={linkNames.path}>{linkName}</Link></li>
-                        <li><Link to={linkNames.path}>{linkName}</Link></li>
-                        <li><Link to={"/link-to"}>{linkName}</Link></li>
-                    </ul>
-                </nav>
-            })}
-        </>
-    );
+            <ul className="container-wrapper">
+                <li><Link to={"/link-to"}>{LinkName}</Link></li>
+                <li><Link to={"/link-to"}>{LinkName}</Link></li>
+                <li><Link to={"/link-to"}>{LinkName}</Link></li>
+            </ul>
+        </nav>
+    )
 }
 
 export default NavbarV1;
