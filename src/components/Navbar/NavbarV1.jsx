d174560ee8ecb86f03b7862f164a0a9c4573816a
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 17:31:14 +0800

    Subject: Update

--- src/components/Navbar/NavbarV1.jsx
+++ src/components/Navbar/NavbarV1.jsx
@@ -4,7 +4,7 @@ import { useState } from "react";
 function NavbarV1() {
 
     const [linkName] =  useState([
-        { id: 1, imgLogo: '/path-img' ,linkName: "Sample1"}
+        { id: 1, imgLogo: '/path-img' ,linkName: "Sample1", path: '/path-to'}
     ]);
 
 
@@ -16,14 +16,14 @@ function NavbarV1() {
                     <img src={linkNames.imgLogo} alt="Logo" />
 
                     <ul className="container-wrapper">
-                        <li><Link to={"/link-to"}>{linkName}</Link></li>
+                        <li><Link to={linkNames.}>{linkName}</Link></li>
                         <li><Link to={"/link-to"}>{linkName}</Link></li>
                         <li><Link to={"/link-to"}>{linkName}</Link></li>
                     </ul>
                 </nav>
             })}
         </>
-    ) ;
+    );
 }
 
 export default NavbarV1;
