0aca7fb8832fef5809190711192b51444a0105de
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 16:03:40 +0800

    Subject: Update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route />
+          <Route path="/" element={ <} />
         </Routes>
       </BrowserRouter>
     </>
--- src/pages/LandingScreen.jsx
+++ src/pages/LandingScreen.jsx
@@ -2,7 +2,7 @@
 function LandingScreen() {
     return (
         <div>
-
+            <h1>Hello World</h1>
         </div>
     )
 }
