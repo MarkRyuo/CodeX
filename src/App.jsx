f991d7f0b72e489b414617b467216e433d3530e5
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-08 16:03:55 +0800

    Subject: Update

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router";
 
 import './App.css'
+import LandingScreen from "./pages/LandingScreen";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
-          <Route path="/" element={ <} />
+          <Route path="/" element={ <LandingScreen />} />
         </Routes>
       </BrowserRouter>
     </>
