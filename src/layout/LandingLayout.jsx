a4c858b745b0dee42968242c9f32de0b760392b1
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-09 23:57:40 +0800

    Subject: Update

--- src/layout/LandingLayout.jsx
+++ src/layout/LandingLayout.jsx
@@ -1,11 +1,9 @@
 import PropTypes from 'prop-types'; // ES6
-import NavBarV1 from '../components/Navbar/NavbarV1'
 
 function LandingLayout({children}) {
     return (
         <div>
             
-            <NavBarV1 />
             <div className="LandingContainer">
                 {children}
             </div>
