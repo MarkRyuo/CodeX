60a2fdb7e9910637c14a3f1a9279473ae54a00be
MarkRyuo <malupajhonmark3035@gmail.com>
2024-12-10 22:22:11 +0800

    Subject: Update

--- src/components/Navbar/NavbarV1.jsx
+++ src/components/Navbar/NavbarV1.jsx
@@ -8,7 +8,7 @@ function NavbarV1() {
 
     return (
         <nav className="navbar-container">
-            <img src="path-to-logo" alt="Logo" />
+            <img src="path-to-logo" alt="Logo" className="navbar-logo-img"/>
 
             <ul className="container-wrapper">
                 <li><Link to={"/link-to"}>{LinkName}</Link></li>
